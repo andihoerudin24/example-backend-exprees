ff23360dcfed949a9080798424c4c9057f7d5848
Andi Hoerduin <andihoerudin24@gmail.com>
2021-10-16 19:23:45 +0700

    setup dockerfile

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Building New Image ..."
 docker build -f Dockerfile.dev -t backend:dev .
 
 echo "Starting Container"
-docker run -d -p 8081:8081 --name backend backend:dev
+docker run -d -p 8080:8080 --name backend backend:dev
 
 echo "Setup Timezone"
 docker run -e TZ=Asia/Jakarta debian:jessie date
